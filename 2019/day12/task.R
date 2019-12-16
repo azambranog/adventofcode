3ff52f13f5d4d8ef417505a6d649d5b9cacc3357
Zambrano Garcia Andres Alejandro (TT-RHC/ECP2) <ndrszmbrn@gmail.com>
2019-12-16 09:17:22 +0100

    day 12 part 2

--- 2019/day12/task.R
+++ 2019/day12/task.R
@@ -1,4 +1,5 @@
 library(data.table)
+library(pracma)
 
 apply_gravity <- function(moons) {
   n_moons <- nrow(moons)
@@ -29,8 +30,6 @@ calculate_energy <- function(moons) {
 }
 
 
-
-
 moons <- readLines('moons.txt')
 
 x <- as.numeric(gsub('<x=(.+), y=(.+), z=(.+)>', '\\1', moons))
@@ -40,17 +39,43 @@ z <- as.numeric(gsub('<x=(.+), y=(.+), z=(.+)>', '\\3', moons))
 moons <- data.frame(x = x, y=y, z=z, vx=0, vy=0, vz=0)
 
 
-
 #task1
 steps <- 1000
 for (s in 1:steps) {
-  if (s%%100 == 0){
+  if (s%%1000 == 0){
     message(sprintf('Step: %s', s))
   } 
   moons <- apply_gravity(moons)
 }
 e <- calculate_energy(moons)
 message(sprintf('The Total energy after step %s is %s', s, e))
+
+
+
+#task2
+moons_ini <- data.frame(x = x, y=y, z=z, vx=0, vy=0, vz=0)
+
+moons <- data.frame(x = x, y=y, z=z, vx=0, vy=0, vz=0)
+s <- 0
+cycle_found <- rep(0, 3)
+while (!all(cycle_found != 0)){
+  if (s%%1000 == 0){
+    message(sprintf('Step: %s', s))
+  }
+  
+  s <- s +1
+  moons <- apply_gravity(moons)
+  
+  for (i in 1:3){
+    if (all(moons[, c(i, i+3)] == moons_ini[, c(i, i+3)])) {
+      cycle_found[i] <- s
+    }
+  }
+}
+
+message('Cycles for each coordinate found')
+message(sprintf('The cycles are: %s, %s, %s', cycle_found[1], cycle_found[2], cycle_found[3]))
+message(sprintf('The overall cycle is: %s', Lcm(cycle_found[1], Lcm(cycle_found[2], cycle_found[3]))))
 
 
 
