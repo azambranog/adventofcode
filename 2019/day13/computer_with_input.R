9edb11e2e0f34c42a9856d653d5ec08ba6f7d105
azambranog <ndrszmbrn@gmail.com>
2019-12-17 21:30:17 +0100

    day 13 autoplay

---
+++ 2019/day13/computer_with_input.R
@@ -0,0 +1,130 @@
+computer_with_input <- function(x, pointer_position, relative_base_position, 
+                                inputs, stopmode=F){
+  y <- x
+  pointer <- pointer_position
+  relative_base <- relative_base_position
+  opcode <- y[pointer] %% 100
+  inputs_given <- 0
+  current_result <- NA
+  finished <- TRUE
+  waiting_input <- FALSE
+  while (opcode != 99){
+    if(!opcode %in% 1:9) {
+      message('ERROR BAD OPCODE')
+      return(NULL) 
+    }
+    
+    #nicestuff to get the operator values
+    if (opcode %in% c(1,2,7,8)) {
+      nargs <- 3
+    } else if (opcode %in% c(5,6)) {
+      nargs <- 2
+    } else if (opcode %in% c(3,4,9)) {
+      nargs <- 1
+    }
+    modes <- numeric(nargs)
+    values <- numeric(nargs)
+    indexes <- numeric(nargs)
+    cumval <- y[pointer]
+    for (i in length(modes):1) {
+      modes[i] <- cumval %/% (10^(1 + i))
+      cumval <- cumval - (modes[i]*10^(1+i))
+      
+      if (modes[i] == 0) {
+        indexes[i] <- y[pointer + i] +1
+      } else if (modes[i] == 1) {
+        indexes[i] <- pointer + i
+      } else if (modes[i] == 2) {
+        indexes[i] <- y[pointer + i] + relative_base + 1
+      } 
+      else {
+        message('ERROR BAD MODE')
+        return(NULL)
+      }
+      values[i] <- y[indexes[i]]
+    }
+    #workaround for "dynamic mem"
+    values[is.na(values)] <-0
+    
+    #operations
+    if (opcode == 1){
+      y[indexes[3]] <- values[1] + values[2]
+      pointer <- pointer + 4
+    } 
+    else if (opcode == 2) {
+      y[indexes[3]] <- values[1] *  values[2]
+      pointer <- pointer + 4
+    } 
+    else if (opcode == 3) {
+      if(all(is.na(inputs)) & stopmode){
+        waiting_input <- TRUE
+        break
+      }
+      
+      inputs_given <- inputs_given + 1
+      message('Program requests input')
+      message(sprintf('Using input %s: %s', inputs_given, inputs[inputs_given]))
+      y[indexes[1]] <- inputs[inputs_given]
+      pointer <- pointer + 2
+    } 
+    else if (opcode == 4) {
+      current_result <- y[indexes[1]] 
+      message(sprintf('Output: %s', current_result))
+      pointer <- pointer + 2
+      if(stopmode){
+        break
+      }
+    } 
+    else if (opcode == 5) {
+      if (values[1] != 0) {
+        pointer <- values[2] + 1
+      } else {
+        pointer <- pointer + 3
+      }
+    } 
+    else if (opcode == 6) {
+      if (values[1] == 0) {
+        pointer <- values[2] + 1
+      } else {
+        pointer <- pointer + 3
+      }
+    } 
+    else if (opcode == 7) {
+      if (values[1] < values[2]) {
+        y[indexes[3]] <- 1
+      } else {
+        y[indexes[3]] <- 0
+      }
+      pointer <- pointer + 4
+    } 
+    else if (opcode == 8) {
+      if (values[1] == values[2]) {
+        y[indexes[3]] <- 1
+      } else {
+        y[indexes[3]] <- 0
+      }
+      pointer <- pointer + 4
+    } 
+    else if (opcode == 9) {
+      relative_base <- relative_base + y[indexes[1]]
+      pointer <- pointer + 2
+    }
+    
+    opcode <- y[pointer] %% 100
+    
+  }
+  message(sprintf('Most current result is %s', current_result))
+  
+  if(y[pointer] %% 100 != 99) {
+    finished <- FALSE
+    message('PROGRAM IS HALTED')
+  }
+  message('DONE')
+  
+  
+  return(list(finished=finished, current_output=current_result, 
+              current_pointer=pointer, output_program=y,
+              current_relative_base=relative_base, waiting_input=waiting_input))
+}
+
+
--- 2019/day13/day13.R
+++ 2019/day13/day13.R
@@ -1,61 +1,104 @@
-library(data.table)
-
-source(file.path('..', 'day11', 'computer_with_input.R'))
-source('game_programm.R')
+source('computer_with_input.R')
+source('game_program.R')
 
 
-run_game <- function() {
+run_game <- function(play = F, self_play = F) {
   program <- list(finished = F, current_output = NA, current_pointer = 1, 
-                  output_program = game_programm, current_relative_base = 0)
+                  output_program = game_program, current_relative_base = 0, 
+                  waiting_input =F)
+  if (play) {
+    program$output_program[1] <- 2
+  }
   
-  draw <- data.table(x = numeric(0), y = numeric(0), type = numeric(0))
+  draw <- list()
   
   score <- 0
-  
+  input <- NA
+
   while(!program$finished){
     
     step <- list(x=0, y=0, type=0)
     for (s in c('x', 'y', 'type')){
+      
       suppressMessages(
         program <- computer_with_input(program$output_program, 
                                        program$current_pointer, 
                                        program$current_relative_base, 
-                                       color, stopmode=T)
+                                       c(input), stopmode=T)
       )
+      
+      if(program$waiting_input) {
+        draw_board(list(board = draw, score = score), clear = T)
+        if (self_play) {
+          ball <- get_ball_pos(draw)
+          bar <- get_bar_pos(draw)
+          input <- sign(ball-bar)
+          break
+          
+        } else {
+          input <- readline('move >')
+          if(input == 'a') {input <- -1}
+          else if(input %in% c('s', 'l')) {input <- 1}
+          else {input <- 0}
+          break
+        }
+      } else {
+        input <- NA
+      }
+      
       step[[s]] <- program$current_output
     }
     if (program$finished) {
       break
+    } else if (program$waiting_input) {
+      next
     } else if (step$x == -1 & step$y == 0) {
       score <- step$type
     } else {
-      draw <- unique(rbind(draw, as.data.table(step)), by = c('x', 'y'), fromLast = TRUE)
+      if(length(draw) < (step$y + 1)) {
+        draw[[step$y + 1]] <- c(as.numeric(NA))
+      }
+      type <- step$type
+      if (type == 0) {type_c <- ' '}
+      if (type == 1) {type_c <- '#'}
+      if (type == 2) {type_c <- 'x'}
+      if (type == 3) {type_c <- '_'}
+      if (type == 4) {type_c <- 'o'}
+      draw[[step$y + 1]][[step$x + 1]] <- type_c
     }
   }
+  draw_board(list(board = draw, score = score), clear = T)
   return(list(board = draw, score = score))
 }
 
 
-draw_board <- function(board) {
-  lis <- rep('', max(board$y) + 1)
-  for(i in min(board$y):max(board$y)){
-    li <- rep(' ', max(board$x) + 1)
-    for(j in min(board$x):max(board$x)){
-      type <- board[y == i & x == j, type]
-      if (type == 1) {li[j+1] <- '#'}
-      if (type == 2) {li[j+1] <- 'x'}
-      if (type == 3) {li[j+1] <- '_'}
-      if (type == 4) {li[j+1] <- 'o'}
-    }
-    lis[i+1] <- paste0(li, collapse = '')
+draw_board <- function(results, clear = F) {
+  d <- sapply(results$board, function(x) paste0(x, collapse = ''))
+  d <- paste0(d, collapse = '\n')
+  if (clear) {
+    cat("\014") 
   }
-  cat(paste0(lis, collapse = '\n'))  
+  cat(d)
+  cat(sprintf('\n~~ Score: %s\n', results$score))
 }
-draw_board(results$board)
 
+get_bar_pos <- function(draw) {
+  max(sapply(draw, function(x) max(which(x == '_'))))
+}
 
+get_ball_pos <- function(draw) {
+  max(sapply(draw, function(x) max(which(x == 'o'))))
+}
 
 # task1
 results <- run_game()
-message(sprintf('There are %s tiles on the board', results$board[type == 2, .N]))
-draw_board(results$board)
+message(sprintf('There are %s tiles on the board', 
+                sum(sapply(results$board, function(x) sum(x == 'x')))))
+
+
+#task2
+#results <- run_game(play = T)
+
+#task2 AUTO
+results <- run_game(play = T, self_play = T)
+
